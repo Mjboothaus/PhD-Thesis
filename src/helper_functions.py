f10565ef89c0617fb9a88330b57938fe721f347a
Michael Booth <michael@databooth.com.au>
2022-07-20 14:50:39 +1000

    progress

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,7 +1,18 @@
-from IPython.display import Markdown, display
-from streamlit import markdown
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+from IPython.display import Markdown, display
+from streamlit import markdown
+
+def make_simple_plot(x, y, xlabel, ylabel, title):
+    _, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set(xlabel=xlabel, ylabel=ylabel, title=title)
+    plt.xlim([0, 10])
+    plt.ylim([-20, 40])
+    ax.grid()
+    plt.show()
+    return title
 
 def read_render_markdown_file(markdown_file, output="jupyter"):
     try:
