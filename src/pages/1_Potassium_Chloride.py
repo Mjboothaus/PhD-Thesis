2f28e0c0a3675fdb830ef480569055eebf8c4d8a
Michael Booth <michael@databooth.com.au>
2022-07-21 12:13:21 +1000

    progress

--- src/pages/1_Potassium_Chloride.py
+++ src/pages/1_Potassium_Chloride.py
@@ -4,13 +4,11 @@ from helper_functions import read_render_markdown_file
 
 def create_sidebar():
     st.sidebar.text("Inputs:")
-    st.sidebar.number_input("Temperature (C):", value=25, min_value=-298, max_value=2000)
     st.sidebar.number_input(label=r"$\psi_0$ (mV):", value=0, min_value=-1000, max_value=1000)
-    nPoint = st.sidebar.number_input("nPoint:", value=4096)
-    grid_size = st.sidebar.number_input("Grid size (Angstrom):", value=0.006)
-    z_cutoff = round((nPoint - 1) * grid_size, 4)
-    st.sidebar.text(f"Cutoff (A): {z_cutoff}")
-
+    z_cutoff = st.sidebar.number_input("z_cutoff (A)", value=25, min_value=20, max_value=50)
+    nPoint = st.sidebar.number_input("nPoint:", value=2001)
+    grid_size = z_cutoff / (nPoint - 1)
+    st.sidebar.text(f"Grid size (A): {grid_size}")
     return None
 
 create_sidebar()
