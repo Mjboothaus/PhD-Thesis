3e3beccff089169437af22cbf79cbfd21af75f02
Michael Booth <michael@databooth.com.au>
2022-07-26 17:22:35 +1000

    progress tests

--- src/tests/test_modelling.py
+++ src/tests/test_modelling.py
@@ -18,3 +18,12 @@ def test_calc_l_index():
         for j in range(i, n_component):
             l = calc_l_index(i, j)
             assert l == l_index[l]
+
+
+def test_kappa_kcl():
+    kcl = set_fluid_parameters("kcl")
+    charge = calc_charge(kcl.valence)
+    kcl.beta = calc_beta(kcl.temperature)
+    kcl.epsilon = calc_epsilon(kcl.epsilon_r)
+    kcl.rho = calc_rho(kcl.concentration)
+    assert calc_kappa(kcl.beta, charge, kcl.rho, kcl.epsilon) == pytest.approx(6.77, abs=0.04)
