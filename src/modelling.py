Trying to fix errors in calculation of f1/f2

src/modelling.py
@@ -48,7 +48,8 @@ class Fluid:
 
 fluid_parameters = dict({"kcl": dict({"name": "Potassium Chloride", "component": ["K", "Cl"], "valence": np.array([
     1.0, -1.0]), "charge": np.array([0]), "temperature": 1075.0, "concentration": np.array([19.5, 19.5]),
-    "epsilon_r": 1.0, "index": 1, "charge_pair": np.array([0]), "rho": np.array([0]), "beta": 0.0, "epsilon": 0.0})})
+    "epsilon_r": 1.0, "index": 1, "charge_pair": np.array([0]), "rho": np.array([0]), "beta": 0.0, "epsilon": 0.0,
+    "cr_filename": "nrcg-cr-approx-maybe.dat"})})
 
 
 fluid_parameters["lj1"] = dict({"name": "Lennard-Jones liquid (1-comp)", "component": ["LJ"], "valence": np.array([0.0]), 
@@ -221,14 +222,31 @@ def load_and_interpolate_cr(cr_path, n_point, n_pair, z):
 def integral_z_infty_dr_r_c_short(c_short, n_pair, z, f1):
     for ij in range(n_pair):
         for k, _ in enumerate(z):
-            f1[k, ij] = trapezoid(y=z[:k] * c_short[k, ij], x=z[:k])
+            #f1[k, ij] = trapezoid(y=z[k:] * c_short[k:, ij], x=z[k:])
+            integrand = z*c_short[:, ij]
+            f1[k: ij] = trapezoid(y=integrand[k:], x=z[k:])
     return f1
+
+def calc_f1_integrand(c_short, n_pair, z, n_point):
+    f1_integrand = np.zeros((n_point, n_pair))
+    for ij in range(n_pair):
+        f1_integrand[:, ij] = z * c_short[:, ij]
+    return f1_integrand
+
+
+def calc_f2_integrand(c_short, n_pair, z, n_point):
+    f2_integrand = np.zeros((n_point, n_pair))
+    for ij in range(n_pair):
+        f2_integrand[:, ij] = z*z * c_short[:, ij]
+    return f2_integrand
 
 
 def integral_z_infty_dr_r2_c_short(c_short, n_pair, z, f2):
     for ij in range(n_pair):
         for k, _ in enumerate(z):
-            f2[k, ij] = trapezoid(y=z[:k]*z[:k] * c_short[k, ij], x=z[:k])
+            #f2[k, ij] = trapezoid(y=z[k:]*z[k:] * c_short[k:, ij], x=z[k:])
+            integrand = z*z*c_short[:, ij]
+            f2[k: ij] = trapezoid(y=integrand[k:], x=z[k:])
     return f2
 
 

src/pages/1_🫙_Potassium_Chloride.py
@@ -67,7 +67,9 @@ model = Model(z=z, z_index=z_index, hw=wall_zeros,
 
 from time import sleep
 
-CR_PATH = f"{Path.cwd().as_posix()}/data/nrcg-cr-approx-maybe.dat"
+CR_PATH = f"{Path.cwd().as_posix()}/data/{fluid.cr_filename}"
+
+st.sidebar.text(fluid.cr_filename)
 
 if run_calc := st.button("Run calculation"):
     try:
@@ -82,6 +84,12 @@ if run_calc := st.button("Run calculation"):
     f1 = integral_z_infty_dr_r_c_short(c_short, n_pair, z, model.f1)
     f2 = integral_z_infty_dr_r2_c_short(c_short, n_pair, z, model.f2)
 
+
+    f1_integrand = calc_f1_integrand(c_short, n_pair, z, n_point)
+    f2_integrand = calc_f2_integrand(c_short, n_pair, z, n_point)
+
+    st.write(f1_integrand.shape, f2_integrand.shape)
+
     # initial guess of zero - maybe should be \beta \phi
 
     tw_initial = np.zeros((n_point, n_component))
@@ -89,7 +97,6 @@ if run_calc := st.button("Run calculation"):
 
     model.f1 = f1
     model.f2 = f2
-
 
     # tw_args = (tw, beta_phiw, beta_psi_charge, charge_pair, rho, f1, f2, z,
     #             n_component, n_point, z_index, integral_z_infty, integral_0_z)
@@ -153,14 +160,18 @@ if run_calc := st.button("Run calculation"):
                         xliml=[0, 10], yliml=[-2, 2], title="'Short-range' direct correlation function")
         st.plotly_chart(fig)
 
-
-        fig = plotly_line(z, f1, ["z", "f1_0", "f1_1", "f1_2"], y_label="f1_ij(r)", legend_label="",
-                        xliml=[0, 10], yliml=[-40, 5], title="f1 function")
+        fig = plotly_line(z, f1_integrand, ["z", "F1_0", "F1_1", "F1_2"], y_label="f1_ij(r)", legend_label="", title="f1 function")
         st.plotly_chart(fig)
 
 
-        fig = plotly_line(z, f2, ["z", "f2_0", "f2_1", "f2_2"], y_label="f2_ij(r)", legend_label="",
-                        xliml=[0, 10], yliml=[-40, 5], title="f2 function")
+        fig = plotly_line(z, f2_integrand, ["z", "F2_0", "F2_1", "F2_2"], y_label="f2_ij(r)", legend_label="", title="f2 function")
+        st.plotly_chart(fig)
+
+        fig = plotly_line(z, f1, ["z", "f1_0", "f1_1", "f1_2"], y_label="f1_ij(r)", legend_label="", title="f1 function")
+        st.plotly_chart(fig)
+
+
+        fig = plotly_line(z, f2, ["z", "f2_0", "f2_1", "f2_2"], y_label="f2_ij(r)", legend_label="", title="f2 function")
         st.plotly_chart(fig)
 
 

src/pages/4_🫙_Lennard_Jones.py
@@ -74,6 +74,8 @@ if run_calc := st.button("Run calculation"):
     f1 = integral_z_infty_dr_r_c_short(c_short, n_pair, z, model.f1)
     f2 = integral_z_infty_dr_r2_c_short(c_short, n_pair, z, model.f2)
 
+    assert model.f1.all() == model.f2.all()
+
     # initial guess of zero - maybe should be \beta \phi
 
     tw_initial = np.zeros((n_point, n_component))
@@ -139,17 +141,22 @@ if run_calc := st.button("Run calculation"):
         # st.plotly_chart(fig)
 
 
-        #fig = plotly_line(r, c_short, ["r", "c0"], y_label="c_ij(r)", legend_label="",
-        #                 xliml=[0, 10], yliml=[-2, 2], title="'Short-range' direct correlation function")
-        #st.plotly_chart(fig)
+        fig = plotly_line(r, r*c_short[:, 0], y_col_labels_bulk[:2], y_label="c_ij(r)", legend_label="",
+                         xliml=[0, 10], yliml=[-2, 2], title="'Short-range' direct correlation function")
+        st.plotly_chart(fig)
 
 
-        fig = plotly_line(z, f1, y_col_labels_bulk, y_label="f1_ij(r)", legend_label="",
+        fig = plotly_line(r, r*r*c_short[:, 0], y_col_labels_bulk[:2], y_label="c_ij(r)", legend_label="",
+                         xliml=[0, 10], yliml=[-2, 2], title="'Short-range' direct correlation function")
+        st.plotly_chart(fig)
+
+
+        fig = plotly_line(z, f1, y_col_labels_bulk[:2], y_label="f1_ij(r)", legend_label="",
                          xliml=[0, 10], yliml=[-40, 5], title="f1 function")
         st.plotly_chart(fig)
 
 
-        fig = plotly_line(z, f2, y_col_labels_bulk, y_label="f2_ij(r)", legend_label="",
+        fig = plotly_line(z, f2, y_col_labels_bulk[:2], y_label="f2_ij(r)", legend_label="",
                          xliml=[0, 10], yliml=[-40, 5], title="f2 function")
         st.plotly_chart(fig)
 