12d0e97be63a29565c70b66a7a114b02637bdbfb
Michael Booth <michael@databooth.com.au>
2022-08-07 21:00:10 +1000

    Removing include markdown experiment

--- docs/test_markdown_include/md1.py
+++
@@ -1,9 +0,0 @@
-import markdown
-from markdown_include.include import MarkdownInclude
-
-# Markdown Extensions
-
-markdown_include = MarkdownInclude(
-    configs={'base_path': '/srv/content/', 'encoding': 'iso-8859-1'}
-)
-html = markdown.markdown(source, extensions=[markdown_include])
